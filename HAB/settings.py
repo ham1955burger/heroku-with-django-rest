d084a0ae81b201852ab691e849e0b3f7da9898ba
hyeminOh <hyemin93123@gmail.com>
2016-08-24 15:42:31 +0900

    *

--- HAB/settings.py
+++ HAB/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-#import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,14 +133,14 @@ USE_L10N = True
 USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
-#db_from_env = dj_database_url.config()
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
 
-if os.environ.get('HEROKU'):  # heroku config:set HEROKU=1
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config()
+#if os.environ.get('HEROKU'):  # heroku config:set HEROKU=1
+#    import dj_database_url
+#    DATABASES['default'] = dj_database_url.config()
