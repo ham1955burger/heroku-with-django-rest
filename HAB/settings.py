ae466922e0ff3ad565c00eb1c56c6e648889a766
hyeminOh <hyemin93123@gmail.com>
2016-08-24 15:04:56 +0900

    settings.py -> STATIC_ROOT 추가

--- HAB/settings.py
+++ HAB/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_statics')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
